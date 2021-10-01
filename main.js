f8faf37280e6aea4a7b6912c1e7111e39a30f618
astashkinmisha <trickerforyou@gmail.com>
2021-10-01 17:01:56 +0300

    hw1(edited)

--- main.js
+++ main.js
@@ -4,51 +4,29 @@ const path = require('path');
 const boysPath = path.join(__dirname, 'boys');
 const girlsPath = path.join(__dirname, 'girls');
 
-fs.readdir(boysPath, (err, files) => {
-    if (err) {
-        console.log( err )
-        return;
-    }
-    files.forEach(users =>{
-        fs.readFile(path.join(boysPath, users), (err1, data) => {
-            if(err1){
-                console.log(err1)
-                return;
-            }
-            const value = JSON.parse(data.toString())
-            if(value.gender === 'female'){
-                fs.rename(path.join(boysPath, users), path.join(girlsPath,users), err2 => {
-                    if(err2){
-                        console.log(err2)
-                    }
-                })
-            }
+const moveGenders = (prevPath, gender, newPath) => {
+    fs.readdir(prevPath, (err, files) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        files.forEach(users => {
+            fs.readFile(path.join(prevPath, users), (err1, data) => {
+                if (err1) {
+                    console.log(err1);
+                    return;
+                }
+                const value = JSON.parse(data.toString());
+                if (value.gender === gender) {
+                    fs.rename(path.join(prevPath, users), path.join(newPath, users), err2 => {
+                        if (err2) {
+                            console.log(err2);
+                        }
+                    })
+                }
+            })
         })
     })
-})
-
-
-
-fs.readdir(girlsPath, (err, files) => {
-    if(err){
-        console.log(err)
-        return
-    }
-    files.forEach(users => {
-        fs.readFile(path.join(girlsPath,users), (err1, data) => {
-            if(err1){
-                console.log(err1)
-                return
-            }
-            let value = JSON.parse(data.toString())
-            if(value.gender === "male"){
-                fs.rename(path.join(girlsPath,users), path.join(boysPath,users), err2 => {
-                    if(err2){
-                        console.log(err2)
-                    }
-                })
-            }
-        })
-    })
-})
-
+}
+moveGenders(boysPath, 'female', girlsPath);
+moveGenders(girlsPath, 'male', boysPath);
