d2c75906cc9482eed34d1f96cd844ad234230ccc
astashkinmisha <trickerforyou@gmail.com>
2021-10-08 15:11:49 +0300

    hw3 v1

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const userRouter = require('./routes/user');
+const {PORT, MONGO_URL} = require('./configs/config');
+
+const app = express();
+
+mongoose.connect(MONGO_URL);
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/users', userRouter);
+app.use('/login', userRouter);
+
+app.listen(PORT, () => {
+    console.log(`App listen ${PORT}`);
+});
